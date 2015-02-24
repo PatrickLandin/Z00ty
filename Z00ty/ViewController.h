c65c74a490b4cca6a5c462e6164589aab88d591e
Patrick Landin <patricklandin@gmail.com>
2015-02-24 13:03:56 -0800

    first commit

---
+++ Z00ty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Z00ty
+//
+//  Created by Patrick Landin on 2/24/15.
+//  Copyright (c) 2015 pLandin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Z00ty/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Z00ty
+//
+//  Created by Patrick Landin on 2/24/15.
+//  Copyright (c) 2015 pLandin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
