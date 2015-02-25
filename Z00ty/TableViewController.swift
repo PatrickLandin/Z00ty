89b77be97e88910b25c335a1da1640b92056b1e6
dhourr <daniel.hour@me.com>
2015-02-24 17:30:30 -0800

    json dummy data

--- Z00ty/TableViewController.swift
+++ Z00ty/TableViewController.swift
@@ -18,26 +18,24 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     var refreshControl = UIRefreshControl()
     
+    //pull dummy data from json
+    //organize by all users / all of my upVotes / and photos I've uploaded
+    //then sort each array by most total votes (up - down)
+    
     
     //MARK: DUMMY DATA =====================================================================
     
-    var dummyDataDictionary = DummyData().globalClothingAndVotes
-    
-    var photosIHaveLiked = DummyData().photosIHaveLiked
-    
-    var myUploadedPhotos = DummyData().myUploadedPhotos
-    
-    var imageURLArray = [String]()
-    var votesArray = [Int]()
+    var allPhotos = [Photos]()
+    var allVotes = [Votes]()
+
+    var myPhotos = [Photos]()
+    var tableViewData = [Photos]()
     
     
     //MARK: VIEW DID LOAD ==================================================================
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        imageURLArray = dummyDataDictionary.keys.array
-        votesArray = dummyDataDictionary.values.array
         
         originalTabBarPlacement = tabBarController?.tabBar.frame.origin.y
         
@@ -46,21 +44,86 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         var attributedString = NSAttributedString(string: "Pull to Refresh", attributes: attributes)
         
         refreshControl.attributedTitle = attributedString
-        refreshControl.addTarget(self, action: "fetchAllData", forControlEvents: UIControlEvents.ValueChanged)
+        refreshControl.addTarget(self, action: "pullToRefresh", forControlEvents: UIControlEvents.ValueChanged)
         
         tableView.addSubview(refreshControl)
         
+        loadDataFromJSON()
+        
+        tableViewData = allPhotos
+
+        seperateMyPhotos(allPhotos)
+        
+        sortByVote()
     }
     
-    func fetchAllData() {
+    
+    //MARK: FUNCTIONS GALORE ===============================================================
+    
+    func loadDataFromJSON() {
+        
+        let path = NSBundle.mainBundle().pathForResource("photos", ofType: "json")
+        let jsonData = NSData(contentsOfFile: path!, options: nil, error: nil)
+        var jsonResult = NSJSONSerialization.JSONObjectWithData(jsonData!, options: nil, error: nil) as [AnyObject]
+        
+        for photo in jsonResult {
+            
+            var photos = Photos()
+            photos.phoneId = photo["phoneId"] as String
+            photos.photoUrl = photo["photoUrl"] as String
+            photos.up = photo["up"] as Int
+            photos.down = photo["down"] as Int
+            photos.total = photos.up - photos.down
+            
+            allPhotos.append(photos)
+        }
+    }
+    
+    
+    func loadVoteDataFromJSON() {
+        
+        let path = NSBundle.mainBundle().pathForResource("votes", ofType: "json")
+        let jsonData = NSData(contentsOfFile: path!, options: nil, error: nil)
+        var jsonResult = NSJSONSerialization.JSONObjectWithData(jsonData!, options: nil, error: nil) as [AnyObject]
+        
+        for vote in jsonResult {
+            
+            var votes = Votes()
+            votes.userID = vote["userId"] as String
+            votes.photoURL = vote["photoUrl"] as String
+            votes.registeredVote = vote["registeredVote"] as String
+            
+            allVotes.append(votes)
+        }
+        
+    }
+    
+    
+    func seperateMyPhotos(allPhotos: [Photos]) {
+        for photo in allPhotos {
+            if photo.phoneId == "0" {
+                myPhotos.append(photo)
+            }
+        }
+    }
+    
+    
+    func sortByVote() {
+        
+        tableViewData.sort { (a, b) -> Bool in
+            
+            if a.total > b.total {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+    
+    func pullToRefresh() {
         
         refreshControl.beginRefreshing()
-        
-        dummyDataDictionary = DummyData().photosIHaveLiked
-        photosIHaveLiked = DummyData().myUploadedPhotos
-        myUploadedPhotos = DummyData().globalClothingAndVotes
-        
-        tableView.reloadData()
         
         refreshControl.endRefreshing()
     }
@@ -97,7 +160,7 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
                     height: height!)
             })
             
-            //tabbar reappears
+        //tabbar reappears
         } else {
             UIView.animateWithDuration(0.3, animations: { () -> Void in
                 
@@ -118,22 +181,26 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     //MARK: UISEGMENTED CONTROL ============================================================
     
     @IBAction func segmentedControlAction(sender: UISegmentedControl) {
-        
+
+        switchSegmentedControlIndex()
+    }
+    
+    func switchSegmentedControlIndex() {
         switch segmentedControl.selectedSegmentIndex {
             
         case 0:
-            imageURLArray = dummyDataDictionary.keys.array
-            votesArray = dummyDataDictionary.values.array
+            tableViewData = allPhotos
+            sortByVote()
             tableView.reloadData()
             
         case 1:
-            imageURLArray = photosIHaveLiked.keys.array
-            votesArray = photosIHaveLiked.values.array
+            
+            tableViewData = []
             tableView.reloadData()
             
         case 2:
-            imageURLArray = myUploadedPhotos.keys.array
-            votesArray = myUploadedPhotos.values.array
+            tableViewData = myPhotos
+            sortByVote()
             tableView.reloadData()
             
         default:
@@ -147,7 +214,7 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     //number of cells
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return imageURLArray.count
+        return tableViewData.count
     }
     
     //cell content
@@ -157,14 +224,15 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         cell.rankLabel.text = "#\(indexPath.row + 1)"
         
-        var imageURL = NSURL(string: imageURLArray[indexPath.row])
+        var imageURL = NSURL(string: tableViewData[indexPath.row].photoUrl)
         var data = NSData(contentsOfURL: imageURL!)
         
-        cell.itemImageView?.image = UIImage(data: data!)
+        cell.itemImageView.image = UIImage(data: data!)
         cell.itemImageView?.contentMode = UIViewContentMode.ScaleAspectFill
         cell.itemImageView?.clipsToBounds = true
         
-        cell.totalVotesLabel.text = "\(votesArray[indexPath.row])"
+        cell.totalVotesLabel.text = "\(tableViewData[indexPath.row].up - tableViewData[indexPath.row].down)"
+        
         
         return cell
     }
