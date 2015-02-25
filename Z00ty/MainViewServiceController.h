641bdad72de8d63765a39bc78191ea717c79d2b1
Ryan Christensen <randhyllcho@gmail.com>
2015-02-25 11:42:24 -0800

    got token for the rest of the app to use. basically the most important thing that will ever happen...

---
+++ Z00ty/MainViewServiceController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewServiceController.h
+//  Z00ty
+//
+//  Created by RYAN CHRISTENSEN on 2/24/15.
+//  Copyright (c) 2015 pLandin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainViewServiceController : NSObject
+
+-(void)postPhoneID:(void (^) (NSURL *url, NSString *error))completionHandler;
++(id)sharedService;
+
+@end
